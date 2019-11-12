c86405fcd03c6bcffe58aa2c9152059e3942d84d
LuGeorgiev <lubomir_78@abv.bg>
2019-11-12 16:48:56 +0200

    expresions

---
+++ ExpressionTrees/Demo/Dog.cs
@@ -0,0 +1,23 @@
+﻿namespace Demo
+{
+    public class Dog
+    {
+        public Dog()
+        {
+
+        }
+
+        public Dog(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+
+        public Owner Owner { get; set; }
+
+        public string SayBau(int number)
+        {
+            return $"Bauu {number} times!";
+        }
+    }
+}
