a0cc1b075baea0de532942771773e53bea424beb
LuGeorgiev <lubomir_78@abv.bg>
2019-11-27 22:29:15 +0200

    Multithreading second part

---
+++ Asyncronous/StateMachine/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StateMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello();
+        }
+
+        static async void Hello()
+        { 
+            Console.WriteLine("Hello World!");        
+        }
+    }
+}
